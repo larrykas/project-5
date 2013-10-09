c5a5254a91b59a46a99ad00a8928bb8b7ce67b6e
larrykas <larrykas@users.noreply.github.com>
2013-10-09 08:56:36 +0300

    Create while_until.rb
    while_until loop

---
+++ while_until.rb
@@ -0,0 +1,13 @@
+a = 1
+a *= 2 while a < 100 
+a -= 10 until a < 100 
+# a → 98
+puts a
+
+$i = 1
+$num = 6
+begin
+   puts("This is day #$i" )
+   $i +=1
+end while $i < $num
+puts "Thank you all for coming."
